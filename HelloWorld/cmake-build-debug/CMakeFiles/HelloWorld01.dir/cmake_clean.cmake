6e0bc7fc0ccf01925685bb4aa98003cf20468a97
Ben Haywood <52361841+MarvelousAudio@users.noreply.github.com>
2021-03-16 18:59:26 -0700

    HelloWorld commit
    added comments for understanding.

---
+++ HelloWorld/cmake-build-debug/CMakeFiles/HelloWorld01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld01.dir/main.cpp.obj"
+  "HelloWorld01.exe"
+  "HelloWorld01.exe.manifest"
+  "HelloWorld01.lib"
+  "HelloWorld01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
