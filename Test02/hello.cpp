0d3ccb33f5fe4f5aff16acec0421f5e47a919213
Ben Haywood <52361841+MarvelousAudio@users.noreply.github.com>
2021-03-16 19:24:51 -0700

    test02--is about adding libraries to projects using Cmake

---
+++ Test02/hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ben Haywood on 3/16/2021.
+//
+
+#include "hello.h"
+
+void print_string(const std::string &s)
+{
+    std::cout << s << "\n";
+}
---
+++ Test02/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "hello.h"
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
